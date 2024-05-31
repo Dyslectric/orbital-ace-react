236a5d1c5a401591a44a60385d3239dd8119fd2f
dyslectric <tafyog@gmail.com>
2024-05-31 16:07:15 -0400

    nebula masks

--- src/components/SpaceBackground.jsx
+++ src/components/SpaceBackground.jsx
@@ -18,43 +18,73 @@ export const SpaceBackground = () => {
     const nebulaTextures = useMemo(
         () => [
             Texture.from("./src/assets/bgs/1024/Blue Nebula/Blue_Nebula_01-1024x1024.png"),
-            Texture.from("./src/assets/bgs/1024/Purple Nebula/Purple_Nebula_01-1024x1024.png"),
             Texture.from("./src/assets/bgs/1024/Green Nebula/Green_Nebula_02-1024x1024.png"),
+            Texture.from("./src/assets/bgs/1024/Purple Nebula/Purple_Nebula_01-1024x1024.png"),
         ],
         []
     );
-    //const nebulaMaskTextures = useMemo(
-    //    () => [
-    //        Texture.from("./src/assets/masks/pnoise1.png"),
-    //        Texture.from("./src/assets/masks/pnoise2.png"),
-    //        Texture.from("./src/assets/masks/pnoise3.png"),
-    //    ],
-    //    []
-    //);
-    const starfieldZ = 16;
-    const nebulasZ = useMemo(() => [8, 7, 6], []);
+    const nebulaMaskTextures = useMemo(
+        () => [
+            Texture.from("./src/assets/masks/pnoise1.png"),
+            Texture.from("./src/assets/masks/pnoise2.png"),
+            Texture.from("./src/assets/masks/pnoise3.png"),
+        ],
+        []
+    );
+    const nebulaMasks = useMemo(
+        () => [new Sprite(nebulaMaskTextures[0]), new Sprite(nebulaMaskTextures[1]), new Sprite(nebulaMaskTextures[2])],
+        []
+    );
+    useEffect(() => {
+        nebulaMasks.forEach((mask) => {
+            mask.anchor.set(0.5);
+            app.stage.addChild(mask);
+            //mask.anchor.set(0.5);
+            //mask.scale.set();
+        });
+    });
+    useEffect(() => {
+        nebulaMasks.forEach((mask, index) => {
+            const nebulaDefaultScale = 24;
+            mask.x = width / 2 - x / (z + nebulasZ[index]);
+            mask.y = height / 2 - y / (z + nebulasZ[index]);
+            mask.scale.set(nebulaDefaultScale * 1 / (z + nebulasZ[index]));
+            //mask.width = width;
+            //mask.height = height;
+        });
+    });
+    const starfieldZ = 17;
+    const nebulasZ = useMemo(() => [16, 15.5, 15], []);
 
     return (
         <>
             <Container x={width / 2} y={height / 2}>
                 <TilingSpriteComponent
+                    anchor={0.5}
                     texture={starfieldTexture}
-                    tilePosition={{ x: -x / starfieldZ, y: -y / starfieldZ }}
+                    tilePosition={{
+                        x: width / 2 - x / (z + starfieldZ),
+                        y: height / 2 - y / (z + starfieldZ),
+                    }}
                     width={width}
                     height={height}
-                    tileScale={ 1 / (z + starfieldZ) }
+                    tileScale={4 * 1 / (z + starfieldZ)}
                 />
                 {nebulaTextures.map((texture, index) => (
                     <TilingSpriteComponent
                         anchor={0.5}
                         key={index}
                         texture={texture}
-                        alpha={0.5}
-                        tileScale={ 1 / (z + nebulasZ[index]) }
+                        alpha={1.0}
+                        tileScale={1 / (z + nebulasZ[index])}
                         width={width}
                         height={height}
-                        tilePosition={{x: width / 2 - x / (z + nebulasZ[index]), y: height / 2 - y / (z + nebulasZ[index])}}
-                        //blendMode={BLEND_MODES.ADD}
+                        tilePosition={{
+                            x: width / 2 - x / (z + nebulasZ[index]),
+                            y: height / 2 - y / (z + nebulasZ[index]),
+                        }}
+                        blendMode={BLEND_MODES.ADD}
+                        mask={nebulaMasks[index]}
                     />
                 ))}
             </Container>
